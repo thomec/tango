9ef6dc1d1298591d383d6a9f314ff70f147c2b92
thomec <thomec@gmail.com>
2015-11-16 21:55:38 +0100

    TDD - ch 05 - render items in template

--- lists/views.py
+++ lists/views.py
@@ -12,7 +12,8 @@ def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
-
-    return render(request, 'lists/home.html')
+    
+    items = Item.objects.all()
+    return render(request, 'lists/home.html', {'items': items})
 
 
