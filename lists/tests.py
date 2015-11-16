a1d97171c7c103126fbe1c3187d59f0946f9046b
thomec <thomec@gmail.com>
2015-11-16 21:38:34 +0100

    TDD - ch 05 - separate unittests

--- lists/tests.py
+++ lists/tests.py
@@ -46,14 +46,13 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-        """
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-                'lists/home.html',
-                {'new_item_text': 'A new list item'}
-                )
-        self.assertEqual(response.content.decode(), expected_html)
-        """
+       
+    def test_home_page_redirects_after_post(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
