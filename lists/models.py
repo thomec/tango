72949a5b35de3aa21118f2c5fe37e5382f241594
thomec <thomec@gmail.com>
2015-11-17 22:20:09 +0100

    TDD - ch 06 - added list model

--- lists/models.py
+++ lists/models.py
@@ -4,5 +4,13 @@
 from django.db import models
 
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
+    list = models.ForeignKey(List, default=None)
     text = models.TextField(default='')
+
+
+
