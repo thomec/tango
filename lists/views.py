b2b7223c9bdaa76ff31cf3cb59c4099639e20c79
thomec <thomec@gmail.com>
2015-11-18 18:42:02 +0100

    TDD - ch 06 - unittest urls and views to use different urls for each list

--- lists/views.py
+++ lists/views.py
@@ -10,15 +10,22 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'lists/home.html')
 
+
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    return render(request, 'lists/list.html', {'items': items})
+    # items = Item.objects.filter(list=list_)
+    return render(request, 'lists/list.html', {'list': list_})
 
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id))
+
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
 
 
--- tango/urls.py
+++ tango/urls.py
@@ -27,6 +27,8 @@ from lists import views
 urlpatterns = [
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home_page, name='home_page'),
-    url(r'^lists/(?P<list_id>[0-9]+)/$', views.view_list, name='view_list'),
+    #url(r'^lists/(?P<list_id>[0-9]+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
     url(r'^lists/new$', views.new_list, name='new_list'),
 ]
