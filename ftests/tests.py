dc97a4d952588a4060df65b9707028ad52c34f67
thomec <thomec@gmail.com>
2015-11-19 19:36:04 +0100

    TDD - ch 07 - use bootstrap in templates

--- ftests/tests.py
+++ ftests/tests.py
@@ -4,12 +4,13 @@
 import unittest
 #import time
 
-from django.test import LiveServerTestCase
+#from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
