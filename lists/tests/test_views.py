a5cf15241707fe93e52de7f407b102c3d9f4a4ca
thomec <thomec@gmail.com>
2015-11-21 18:13:48 +0100

    TDD - ch 10 - start refactor out add_item url, modify template and unittest

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -69,6 +69,34 @@ class ListViewTest(TestCase):
         self.assertEqual(response.context['list'], correct_list)
 
 
+    def test_can_save_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_post_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+
+
 class NewListTest(TestCase):
 
     def test_saving_post_request(self):
@@ -101,9 +129,9 @@ class NewListTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-class NewItemTest(TestCase):
+class NewItemTest(TestCase):    # moved to ListViewTest -> can be deleted
 
-    def test_can_save_a_POST_request_to_an_existing_list(self):
+    def test_can_save_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
@@ -118,7 +146,7 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.list, correct_list)
 
 
-    def test_redirects_to_list_view(self):
+    def test_post_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
