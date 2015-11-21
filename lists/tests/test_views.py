ee4dc6268caafa9607eda5adfa6c8c46fe10e85b
thomec <thomec@gmail.com>
2015-11-21 17:54:40 +0100

    TDD - ch 10 - model validation: don't save empty list items

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -91,8 +91,14 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'lists/home.html')
-        expected_error = escape("You can't have an empty list item") # -> You can&#39;t
+        expected_error = escape("You can't have an empty list item!") # -> You can&#39;t
         self.assertContains(response, expected_error)
+    
+    
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class NewItemTest(TestCase):
