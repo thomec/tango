89048c053d2aa3475a58473f7bd260ecb5ff7fc1
thomec <thomec@gmail.com>
2015-11-22 10:58:55 +0100

    TDD - ch 11 - unittest to check use of forms

---
+++ lists/tests/test_forms.py
@@ -0,0 +1,15 @@
+# lists/tests/test_forms.py
+
+
+from django.test import TestCase
+
+from lists.forms import ItemForm
+
+
+class ItemFormTest(TestCase):
+
+    def test_form_renders_item_text_input(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+        self.fail(form.as_p())
