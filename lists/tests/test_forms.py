8f266904f5761fe4dc4c1b4c8b5c5a61eb7cfd6d
thomec <thomec@gmail.com>
2015-11-22 11:47:42 +0100

    TDD - ch 11 - add unittest to check form validation for blank items

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -13,3 +13,12 @@ class ItemFormTest(TestCase):
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
         self.assertIn('class="form-control input-lg"', form.as_p())
         #self.fail(form.as_p())
+
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+                form.errors['text'],
+                ["You can't have an empty list item!"]
+                )
