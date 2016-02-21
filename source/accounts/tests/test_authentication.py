b0574cd37097b8b22f7bdd28b2b4d8dea775066e
thomec <thomec@gmail.com>
2016-02-21 20:40:43 +0100

    TDD - ch 16 Patching at Class Level

--- source/accounts/tests/test_authentication.py
+++ source/accounts/tests/test_authentication.py
@@ -3,27 +3,50 @@
 
 from unittest.mock import patch
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 from accounts.authentication import (
     PERSONA_VERIFY_URL, DOMAIN, PersonaAuthenticationBackend
 )
 
+User = get_user_model()
 
+
+@patch('accounts.authentication.requests.post')
 class AuthenticateTest(TestCase):
 
-    @patch('accounts.authentication.requests.post')
+    def setUp(self):
+        self.backend = PersonaAuthenticationBackend()
+        user = User(email='other@user.com')
+        user.username = 'otheruser'  #1
+        user.save()
+
+    #@patch('accounts.authentication.requests.post')
     def test_sends_assertion_to_mozilla_with_domain(self, mock_post):
-        backend = PersonaAuthenticationBackend()
-        backend.authenticate('an assertion')
+        #backend = PersonaAuthenticationBackend()
+        #backend.authenticate('an assertion')
+        self.backend.authenticate('an assertion')
         mock_post.assert_called_once_with(
             PERSONA_VERIFY_URL,
             data={'assertion': 'an assertion', 'audience': DOMAIN}
         )
 
-    @patch('accounts.authentication.requests.post')
+    #@patch('accounts.authentication.requests.post')
     def test_returns_none_if_response_errors(self, mock_post):
         mock_post.return_value.ok = False
-        backend = PersonaAuthenticationBackend()
-
-        user = backend.authenticate('an assertion')
+        #backend = PersonaAuthenticationBackend()
+        user = self.backend.authenticate('an assertion')
         self.assertIsNone(user)
+
+    def test_returns_none_if_status_not_okay(self, mock_post):
+        mock_post.return_value.json.return_value = {'status': 'not okay!'}  #4
+        user = self.backend.authenticate('an assertion')
+        self.assertIsNone(user)
+
+    def test_finds_existing_user_with_email(self, mock_post):
+        mock_post.return_value.json.return_value = {
+            'status': 'okay', 'email': 'a@b.com'
+        }
+        actual_user = User.objects.create(email='a@b.com')
+        found_user = self.backend.authenticate('an assertion')
+        self.assertEqual(found_user, actual_user)
