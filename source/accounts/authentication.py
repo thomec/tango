62eab7c0ffcb9bc3eb57d8e2a2f6ccac2aa1740c
thomec <thomec@gmail.com>
2016-02-21 21:27:35 +0100

    TDD - ch 16 De-spiking and Mocking out: get_user Method

--- source/accounts/authentication.py
+++ source/accounts/authentication.py
@@ -23,3 +23,9 @@ class PersonaAuthenticationBackend(object):
                 return User.objects.get(email=email)
             except User.DoesNotExist:
                 return User.objects.create(email=email)
+
+    def get_user(self, email):
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            return None
