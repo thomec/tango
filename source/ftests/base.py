1dc1b33f4f1be749668fabd9495b11231535088a
thomec <thomec@gmail.com>
2016-02-26 23:47:33 +0100

    TDD - ch 21 New FT for sharing, move session creation stuff to base

--- source/ftests/base.py
+++ source/ftests/base.py
@@ -5,12 +5,15 @@ import os
 import time
 from unittest import skip
 from datetime import datetime
+from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 
 from .server_tools import reset_database
+from .server_tools import create_session_on_server
+from .management.commands.create_session import create_pre_authenticated_session
 
 SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'screendumps'
@@ -119,3 +122,24 @@ class FunctionalTest(StaticLiveServerTestCase):
                 time.sleep(0.1)
         # one more try, which will raise any errors if they are outstanding
         return function_with_assertion()
+
+    def create_pre_authenticated_session(self, email):
+        """
+        user = User.objects.create(email=email)
+        session = SessionStore()
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+        """
+        if self.against_staging:
+            session_key = create_session_on_server(self.server_host, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+        ## to set a cookie we need to first visit the domain.
+        ## 404 pages load the quickest!
+        self.browser.get(self.server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name = settings.SESSION_COOKIE_NAME,
+            value = session_key,
+            path = '/',
+        ))
