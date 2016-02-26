880f650ec0c149f8d9201557aabcb284686e22db
thomec <thomec@gmail.com>
2016-02-26 19:09:06 +0100

    use wait_for for URL checks in my_lists

--- source/ftests/base.py
+++ source/ftests/base.py
@@ -2,10 +2,12 @@
 
 import sys
 import os
+import time
 from unittest import skip
 from datetime import datetime
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 
 from .server_tools import reset_database
@@ -13,6 +15,7 @@ from .server_tools import reset_database
 SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'screendumps'
 )
+DEFAULT_WAIT = 5
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -38,7 +41,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         if self.against_staging:
             reset_database(self.server_host)
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(DEFAULT_WAIT)
 
     def tearDown(self):
         #self.browser.quit()
@@ -106,3 +109,13 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.wait_for_element_with_id('id_login')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
+
+    def wait_for(self, function_with_assertion, timeout=DEFAULT_WAIT):
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            try:
+                return function_with_assertion()
+            except (AssertionError, WebDriverException):
+                time.sleep(0.1)
+        # one more try, which will raise any errors if they are outstanding
+        return function_with_assertion()
